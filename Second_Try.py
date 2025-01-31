f6a8c7e882eb9d794a4e0ae37a17974ef44c3dca
Alexander Zelenyi <alxzln_corporate@gmail.com>
2025-01-31 13:31:26 +0300

    Removed webdriver_manager

--- Second_Try.py
+++ Second_Try.py
@@ -1,16 +1,19 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 import time
 import pandas as pd
 
-service = Service(executable_path=ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
+
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+# service = Service(executable_path=ChromeDriverManager().install())
+# driver = webdriver.Chrome(service=service)
+
+driver = webdriver.Chrome()
 url = 'https://avia.tutu.ru/'
 
 driver.get(url)
